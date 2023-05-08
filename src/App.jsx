086142365e963be87e92c28f4761787477a8aab1
socram174 <socram174@hotmail.com>
2023-05-08 12:35:40 -0400

    Prueba

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <form action='http://localhost:3001/api/forms' method='POST'>
+        <label htmlFor='name'>Name</label>
+        <input type='text' id='name' name='name' />
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' name='email' />
+
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default App
